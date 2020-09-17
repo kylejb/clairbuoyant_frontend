899c6789e0306fcf5fa88185c09127b5850416f9
kylejb <47502769+kylejb@users.noreply.github.com>
2020-09-17 19:47:59 -0400

    Generated to manage NavBar behavior

---
+++ src/Components/navbar/navbar.routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Route } from 'react-router-dom';
+
+const NavigationBarRouter = props => {
+    // handleUserState={props.handleUserState}
+    return (
+        <>
+            {/* <Route path="/login" render={ ( routerProps ) => <LoginForm {...routerProps} />} /> */}
+            {/* <Route path="/register" render={ ( routerProps ) => <RegistrationForm {...routerProps} />} /> */}
+        </>
+    );
+}
+
+export default NavigationBarRouter;
