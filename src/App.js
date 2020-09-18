8d5de273d0a6fa715ea4365c54288aecc8f4ff1a
kylejb <kjburda@gmail.com>
2020-09-18 14:25:27 -0400

    Minor formatting

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Header from './Components/Header'
 import StaticLayout from './Components/StaticLayout'
 import Footer from './Components/Footer'
 
-// import ForecastContainer from './Containers/ForecastContainer'
-
 function App() {
   return (
     <>
