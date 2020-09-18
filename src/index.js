ab9548210004a58cda549fbde779cf0fd7a323f4
kylejb <kjburda@gmail.com>
2020-09-17 20:20:38 -0400

    Added import for 'semantic-ui-css'

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import history from './history'
 import { Router } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
