45c11afd9b3c4e8dd3b1c2f7029952238bf00605
kylejb <47502769+kylejb@users.noreply.github.com>
2020-09-17 20:14:37 -0400

    Added Route to UserLoginForm

--- src/Components/navbar/navbar.routes.jsx
+++ src/Components/navbar/navbar.routes.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import UserLoginForm from '../forms/UserLoginForm';
 
 const NavigationBarRouter = props => {
     // handleUserState={props.handleUserState}
     return (
         <>
-            {/* <Route path="/login" render={ ( routerProps ) => <LoginForm {...routerProps} />} /> */}
+            <Route path="/login" render={ ( routerProps ) => <UserLoginForm {...routerProps} />} />
             {/* <Route path="/register" render={ ( routerProps ) => <RegistrationForm {...routerProps} />} /> */}
         </>
     );
