1c40b03c9f13ba5bef7e4a7013ec7f9616f0ef08
kylejb <47502769+kylejb@users.noreply.github.com>
2020-09-20 18:12:29 -0400

    Added dynamic onClick client-side routing; requires additional refactoring... brittle due to state management, component layout

--- src/Components/HeaderSearch.jsx
+++ src/Components/HeaderSearch.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { Dropdown } from 'semantic-ui-react';
 
 //* add country, state to db
@@ -12,7 +13,7 @@ import { Dropdown } from 'semantic-ui-react';
 const renderBeachOption = (beachObj) => {
   return { key: beachObj.id, value: beachObj.name, flag: 'us', text: beachObj.name }
 }
-const HeaderDropDownSearch = ( { beaches } ) => {
+const HeaderDropDownSearch = ( { beaches, selectedBeachHelper, history } ) => {
   
   const beachOptions = () => {
     if (beaches){
@@ -20,15 +21,26 @@ const HeaderDropDownSearch = ( { beaches } ) => {
     }
   }
   
-  console.log("HDDS ", beaches)
+  const onCloseHelper = (e) => {
+    if (e) {
+      const selectedBeach = beaches.filter(beachObj => beachObj.name === e.target.textContent)
+      const urlPrefix = selectedBeach[0].name.replace(/\s+/g, '-').toLowerCase();
+      selectedBeachHelper(selectedBeach[0])
+      history.push(`/forecast/${urlPrefix}`)
+    } else {
+      return null
+    }
+  }
+  
   return (
     <Dropdown
       placeholder='Select Beach'
       search
       selection
       options={beaches ? beachOptions() : null}
+      onClose={onCloseHelper}
     />
   )
 }
 
-export default HeaderDropDownSearch;
+export default withRouter(HeaderDropDownSearch);
