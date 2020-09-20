10317a1094b6e2e6ea8f129eacd8450a07deb939
kylejb <kjburda@gmail.com>
2020-09-20 01:07:44 -0400

    Temporarily saving this copy that is primarily copied from semantic-ui-react's 'Standard Custom Search' example to study Reducer and Hook behavior

---
+++ src/Components/BeachSearch_semantic.jsx
@@ -0,0 +1,119 @@
+import _ from 'lodash'
+import React from 'react'
+import { Search, Grid, Header, Segment, Label } from 'semantic-ui-react'
+
+let source = []
+
+// const source1 = () => {
+//   fetch("http://localhost:3000/api/v1/beaches")
+//   .then(res=>res.json())
+//   .then(data => {
+//     console.log("fetch data ", data)
+//     let newArr = source
+//     newArr = data
+//     source = newArr
+//     console.log("fetch source", source)
+//   })
+// }
+
+// source1();
+
+
+
+const initialState = {
+  loading: false,
+  results: [],
+  value: '',
+}
+
+function exampleReducer(state, action) {
+  switch (action.type) {
+    case 'CLEAN_QUERY':
+      return initialState
+    case 'START_SEARCH':
+      return { ...state, loading: true, value: action.query }
+    case 'FINISH_SEARCH':
+      return { ...state, loading: false, results: action.results }
+    case 'UPDATE_SELECTION':
+      return { ...state, value: action.selection }
+
+    default:
+      throw new Error()
+  }
+}
+
+const resultRenderer = ({ title }) => <Label content={title} />
+
+function SearchExampleStandardCustom() {
+  const [state, dispatch] = React.useReducer(exampleReducer, initialState)
+  const { loading, results, value } = state
+
+  const timeoutRef = React.useRef()
+  const handleSearchChange = React.useCallback((e, data) => {
+    clearTimeout(timeoutRef.current)
+    dispatch({ type: 'START_SEARCH', query: data.value })
+
+    timeoutRef.current = setTimeout(() => {
+      if (data.value.length === 0) {
+        dispatch({ type: 'CLEAN_QUERY' })
+        return
+      }
+
+      const re = new RegExp(_.escapeRegExp(data.value), 'i')
+      const isMatch = (result) => re.test(result.title)
+
+      dispatch({
+        type: 'FINISH_SEARCH',
+        results: _.filter(source, isMatch),
+      })
+    }, 300)
+  }, [])
+  React.useEffect(() => {
+    return () => {
+      clearTimeout(timeoutRef.current)
+    }
+  }, [])
+
+  // const resultsHelper = () => {
+  //   props.beachSearch(results)
+  // }
+
+  return (
+    <>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <Grid>
+      <Grid.Column width={6}>
+        <Search
+          loading={loading}
+          onResultSelect={(e, data) =>
+            dispatch({ type: 'UPDATE_SELECTION', selection: data.result.title })
+          }
+          onSearchChange={handleSearchChange}
+          resultRenderer={resultRenderer}
+          results={results}
+          value={value}
+        />
+      </Grid.Column>
+
+      <Grid.Column width={10}>
+        <Segment>
+          <Header>State</Header>
+          <pre style={{ overflowX: 'auto' }}>
+            {JSON.stringify({ loading, results, value }, null, 2)}
+          </pre>
+          <Header>Options</Header>
+          <pre style={{ overflowX: 'auto' }}>
+            {JSON.stringify(source, null, 2)}
+          </pre>
+        </Segment>
+      </Grid.Column>
+    </Grid>
+    </>
+  )
+}
+
+export default SearchExampleStandardCustom
