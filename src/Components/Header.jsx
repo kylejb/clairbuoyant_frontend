63ef91ba32f500560e00b7ae44207b86e1e621bc
kylejb <47502769+kylejb@users.noreply.github.com>
2020-09-20 14:35:27 -0400

    Hardcoded labels and routes to build and test core functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import HeaderSearch from './HeaderSearch';
 import { Link } from 'react-router-dom';
 import { Container, Dropdown, Image, Menu } from 'semantic-ui-react';
 
@@ -11,24 +12,32 @@ const Header = props => {
                 Sea State
             </Menu.Item>
             <Menu.Item as={Link} to='/home'>Home</Menu.Item>
-            <Dropdown item simple text='Dropdown'>
+            <Dropdown item simple text='Forecasts'>
             <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Item>Placeholder List Items</Dropdown.Item>
+                <Dropdown.Item>Favorites</Dropdown.Item>
                 <Dropdown.Divider />
-                <Dropdown.Header>Header Item</Dropdown.Header>
+                <Dropdown.Header>Regions</Dropdown.Header>
                 <Dropdown.Item>
-                <i className='dropdown icon' />
-                <span className='text'>Submenu</span>
+                    <i className='dropdown icon' />
+                    <span className='text'>USA</span>
+                    <Dropdown.Menu>
+                        <Dropdown.Item as={Link} to='/rockawaybeach-surf-report'>Rockaway Beach, NY</Dropdown.Item>
+                        <Dropdown.Item as={Link} to='/longbeach-surf-report'>Long Beach, NY</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    <i className='dropdown icon' />
+                    <span className='text'>Canada</span>
                 <Dropdown.Menu>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
+                    <Dropdown.Item as={Link} to='/tofino-surf-report'>Tofino, BC</Dropdown.Item>
                 </Dropdown.Menu>
                 </Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
+                {/* <Dropdown.Item>Placeholder List Item</Dropdown.Item> */}
             </Dropdown.Menu>
             </Dropdown>     
             <Menu.Item as={Link} to='/login'>Sign-in</Menu.Item>
+            <Menu.Item> <HeaderSearch /> </Menu.Item>
             </Menu>
         </Container>
     );
