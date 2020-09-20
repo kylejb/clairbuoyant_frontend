9b5376dd45e148a6b9919695f6757a7c2a77ae4a
kylejb <47502769+kylejb@users.noreply.github.com>
2020-09-20 15:33:45 -0400

    Dynamically render list of available 'named beaches'; this will be used to redirect to its showpage for forecast dashboard

--- src/Components/HeaderSearch.jsx
+++ src/Components/HeaderSearch.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import { Dropdown } from 'semantic-ui-react';
 
-// add country, state to db
-const beachOptions = [
-  // { key: 'globe', value: 'globe', flag: 'world', text: 'Earth' },
-  { key: 'rbny', value: 'rockaway beach', flag: 'us', text: 'Rockaway Beach, NY' },
-  { key: 'lbny', value: 'long beach', flag: 'us', text: 'Long Beach, NY' },
-  { key: 'tobc', value: 'tofino', flag: 'ca', text: 'Tofino, BC' },
-]
+//* add country, state to db
+// const beachOptions = [
+//   // { key: 'globe', value: 'globe', flag: 'world', text: 'Earth' },
+//   { key: 'rbny', value: 'rockaway beach', flag: 'us', text: 'Rockaway Beach, NY' },
+//   { key: 'lbny', value: 'long beach', flag: 'us', text: 'Long Beach, NY' },
+//   { key: 'tobc', value: 'tofino', flag: 'ca', text: 'Tofino, BC' },
+// ]
 
-const HeaderDropDownSearch = () => (
-  <Dropdown
-    placeholder='Select Beach'
-    search
-    selection
-    options={beachOptions}
-  />
-)
+const renderBeachOption = (beachObj) => {
+  return { key: beachObj.id, value: beachObj.name, flag: 'us', text: beachObj.name }
+}
+const HeaderDropDownSearch = ( { beaches } ) => {
+  
+  const beachOptions = () => {
+    if (beaches){
+      return beaches.map(namedBeach => renderBeachOption(namedBeach))
+    }
+  }
+  
+  console.log("HDDS ", beaches)
+  return (
+    <Dropdown
+      placeholder='Select Beach'
+      search
+      selection
+      options={beaches ? beachOptions() : null}
+    />
+  )
+}
 
 export default HeaderDropDownSearch;
