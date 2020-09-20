bae163f977508210b910869ff78c62f68d0bdcdc
kylejb <47502769+kylejb@users.noreply.github.com>
2020-09-20 01:04:20 -0400

    Base Search Form prior to 'semantic-ui-react'

---
+++ src/Components/BeachSearch_copy.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const BeachSearch = props => {
+    const [beachSearchValue, setBeachSearchValue] = useState("");
+
+    const searchSubmitHelper = (e) => {
+        e.preventDefault();
+        props.beachSearch(beachSearchValue)
+    }
+
+    
+    return (
+        <form onSubmit={searchSubmitHelper}>
+            <br/>
+            <br/>
+            <br/>
+            <input value={beachSearchValue} onChange={(e) => setBeachSearchValue(e.target.value)}></input>
+            <input type="submit" value="Search"></input>
+        </form>
+    )
+}
+
+export default BeachSearch;
