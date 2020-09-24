e626a18a4896b74e18a82fd02f41c04f2937bd85
kylejb <47502769+kylejb@users.noreply.github.com>
2020-09-24 15:16:31 -0400

    MVP Refactor: dynamic rendering of beach's forecast data sourced from local buoy

--- src/Components/forecast/Wave.component.jsx
+++ src/Components/forecast/Wave.component.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-const TempCard = props => {
+const TideCard = props => {
 
     return (
         <>
-            <h1>Temp Chart</h1>
+            <h1>Wave Height</h1>
    
-            <div className="card-heading"> {props.temp}˚C </div>
+            <div className="card-heading"> {props.wave}m </div>
             <div className="card-value mt-4"> 
             </div>
             
         </>
-     
     );
 }
 
-export default TempCard;
+export default TideCard;
