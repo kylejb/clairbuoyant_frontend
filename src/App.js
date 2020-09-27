d59429ac49b9819e20774dc1890ccb8eb4234c0e
kylejb <kjburda@gmail.com>
2020-09-27 14:36:25 -0400

    Temporary changes while leaflet is incorporated to App

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import Header from './Components/Header';
 import StaticLayout from './Components/StaticLayout';
+import MapContainer from './Containers/MapContainer';
+// import styled from 'styled-components';
 // import Footer from './Components/Footer'
-import ForecastGlobe from './Components/worldmap/Globe';
+// import ForecastGlobe from './Components/worldmap/Globe';
 import LoginForm from './Components/forms/UserLoginForm'
 
-const App = () => {
-  const [selectedBeach, setSelectedBeach] = useState(null),
-        [currentUserToken, setCurrentUserToken] = useState(null);
-  
-  const selectedBeachHelper = (beachObj) => {
-    setSelectedBeach(beachObj)
-  }
 
-  return (
-    <>
-      <Header selectedBeachHelper={selectedBeachHelper} userToken={currentUserToken} />
-      <Route exact path="/home" component={ForecastGlobe} />
-      <StaticLayout beach={selectedBeach} userToken={currentUserToken} />
-      {/* <Footer /> */}
-      <Route exact path="/login" render={() => <LoginForm setToken={setCurrentUserToken}/>} />
-    </>
-  );
+
+export default class App extends React.Component {
+  // const [selectedBeach, setSelectedBeach] = useState(null),
+  //       [currentUserToken, setCurrentUserToken] = useState(null);
+  
+  // const selectedBeachHelper = (beachObj) => {
+  //   setSelectedBeach(beachObj)
+  // }
+
+  render(){
+      return (
+        <>
+      
+            <MapContainer />
+       
+          {/* <Header selectedBeachHelper={selectedBeachHelper} userToken={currentUserToken} /> */}
+          {/* <Route exact path="/home" component={ForecastGlobe} /> */}
+          {/* <StaticLayout beach={selectedBeach} userToken={currentUserToken} /> */}
+          {/* <Footer /> */}
+          {/* <Route exact path="/login" render={() => <LoginForm setToken={setCurrentUserToken}/>} /> */}
+        </>
+      );
+  }
 }
 
-export default App;
