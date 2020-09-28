e0856f58a97270a77623d94d605e812c2821bcdf
kylejb <kjburda@gmail.com>
2020-09-28 17:24:46 -0400

    Refactored to make StaticLayout the parent for the MVP segment of the application; should help with scalability over time

--- src/Components/StaticLayout.jsx
+++ src/Components/StaticLayout.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import MapContainer from '../Containers/MapContainer';
+import ForecastGlobe from './worldmap/Globe';
 import BeachContainer from '../Containers/BeachContainer';
 import JournalContainer from '../Containers/JournalContainer';
-import WorldMap from './worldmap/WorldMap';
+
 
 const StaticLayout = props => {
   // const currentUser = JSON.parse(localStorage.getItem('loggedIn'));
+
 
   return (
     <>
       <Route path='/forecast' render={() => <BeachContainer beach={props.beach} />} />
       <Route path='/journal' render={() => <JournalContainer />} />
-      {/* <WorldMap /> */}
+      <Route exact path="/map" component={MapContainer} />
+      <Route exact path="/home" component={ForecastGlobe} />
     </>
   );
 }
