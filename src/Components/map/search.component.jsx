27c6a98849731c352b07c8d694b9e83106723bc2
kylejb <47502769+kylejb@users.noreply.github.com>
2020-09-27 22:49:19 -0400

    Added ReactLeafletSearch component, which has access to lat-long coordinates in response to string input

---
+++ src/Components/map/search.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactLeafletSearch from 'react-leaflet-search';
+
+const Search = props => {
+
+    return <ReactLeafletSearch position="topleft" />;
+}
+
+export default Search;
