3cc5da5eb0bc3d17fefb953b9f3340dce0d6197d
kylejb <47502769+kylejb@users.noreply.github.com>
2020-09-28 23:34:09 -0400

    Minor formatting and removed unneeded components for MVP

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -27,41 +27,42 @@ const Header = props => {
         return (<Dropdown.Item key={beachObj.id} onClick={() => props.selectedBeachHelper(beachObj)} as={Link} to={`/forecast/${urlPrefix}`}>{beachObj.name}, NY</Dropdown.Item>)
     }
 
+
     return (
-            <Menu fixed='top' stackable inverted>
-                <Container>
-                    <Menu.Item as='a' header>
-                    <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
+        <Menu fixed='top' stackable inverted>
+            <Container>
+                <Menu.Item as='a' header>
+                <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
                     the Beach Buoys!
-                    </Menu.Item>
-                    <Menu.Item as={Link} to='/home'>Home</Menu.Item>
-                    <Dropdown item simple text='Forecasts'>
+                </Menu.Item>
+                <Menu.Item as={Link} to='/home'>Home</Menu.Item>
+                <Dropdown item simple text='Forecasts'>
+                <Dropdown.Menu>
+                    <Dropdown.Item>
+                        <i className='dropdown icon' />
+                        <span className='text'>Surf Log</span>
                     <Dropdown.Menu>
-                        <Dropdown.Item>
-                            <i className='dropdown icon' />
-                            <span className='text'>Surf Log</span>
-                        <Dropdown.Menu>
-                            <Dropdown.Item as={Link} to='/journal/new'>New Entry</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/journal'>All Entries</Dropdown.Item>
-                        </Dropdown.Menu>
-                        </Dropdown.Item>
-                        <Dropdown.Item as={Link} to='/map'>2D Map View</Dropdown.Item>
-                        <Dropdown.Item>Favorites</Dropdown.Item>
-                        <Dropdown.Divider />
-                        <Dropdown.Header>Regions</Dropdown.Header>
-                        <Dropdown.Item>
-                            <i className='dropdown icon' />
-                            <span className='text'>USA</span>
-                            <Dropdown.Menu>
-                                {parseUSANamedBeaches()}
-                            </Dropdown.Menu>
-                        </Dropdown.Item>
+                        <Dropdown.Item as={Link} to='/journal/new'>New Entry</Dropdown.Item>
+                        <Dropdown.Item as={Link} to='/journal'>All Entries</Dropdown.Item>
                     </Dropdown.Menu>
-                    </Dropdown>     
-                    <Menu.Item as={Link} to='/login'>Sign-in</Menu.Item>
-                    <Menu.Item> <HeaderSearch beaches={namedBeaches} selectedBeachHelper={props.selectedBeachHelper}/> </Menu.Item>
-                </Container>
-            </Menu>
+                    </Dropdown.Item>
+                    <Dropdown.Item as={Link} to='/map'>2D Map View</Dropdown.Item>
+                    <Dropdown.Item>Favorites</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Header>Regions</Dropdown.Header>
+                    <Dropdown.Item>
+                        <i className='dropdown icon' />
+                        <span className='text'>USA</span>
+                        <Dropdown.Menu>
+                            {parseUSANamedBeaches()}
+                        </Dropdown.Menu>
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+                </Dropdown>
+                <Menu.Item as={Link} to='/login'>Sign-in</Menu.Item>
+                <Menu.Item> <HeaderSearch beaches={namedBeaches} selectedBeachHelper={props.selectedBeachHelper}/> </Menu.Item>
+            </Container>
+        </Menu>
     );
 }
 
