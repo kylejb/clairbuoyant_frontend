627f2ec7fc692119ccbbbe1ba9375d7c3bc48896
kylejb <47502769+kylejb@users.noreply.github.com>
2020-09-30 15:20:10 -0400

    Added basic login process for demo

--- src/Components/forms/UserLoginForm.jsx
+++ src/Components/forms/UserLoginForm.jsx
@@ -1,36 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react';  
+import { Button, Form, Header, Image, Message, Input, Container } from 'semantic-ui-react';
 
 const UserLoginForm = props => {
     const [email, setEmail] = useState(""),
     [password, setPassword] = useState("");
 
-    const handleSubmitHelper = (e) => {
-        e.preventDefault();
-        console.log("LoginForm Submitted")
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify({ user: { email: email, password: password } })
-        }
-        fetch("http://localhost:3000/api/v1/login", options).then(res=>res.json()).then(user => {
-            // localStorage for MVP only
-            localStorage.setItem("loggedIn", JSON.stringify(user.jwt));
-            props.setToken(user.jwt);
-        })
-    }
-
+     
     return (
-        <>
-            <h2>LoginForm</h2>
-            <form onSubmit={handleSubmitHelper}>
-                <input type="text" placeholder="email address" value={email} onChange={e => setEmail(e.target.value)} />
-                <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
-                <input type="submit" />
-            </form>
-        </>
+
+       <Container>
+          <Header as='h2' color='teal' textAlign='center'>
+            <Image src='/logo.png' /> Login to your account
+          </Header>
+          <Form>
+            <Form.Group width="equal">
+              <Form.Field 
+                control={Input} 
+                icon='user' 
+                iconPosition='left' 
+                label='email'
+                placeholder='E-mail address'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                 />
+              <Form.Field
+                control={Input}
+                icon='lock'
+                iconPosition='left'
+                type='password'
+                label='password'
+                placeholder='Password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+    
+              <Button 
+                color='teal' 
+                size='large'
+                onClick={() => props.handleUserLogin(email, password)}
+              >
+                Login
+              </Button>
+            </Form.Group>
+          </Form>
+          <Message>
+            {/* eslint-disable-next-line */}
+            New to us? <a>Sign Up</a>
+          </Message>
+      </Container>
     );
 }
 
