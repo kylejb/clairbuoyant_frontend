de773a623195b75cf5fb695637bb395469d801cd
kylejb <47502769+kylejb@users.noreply.github.com>
2020-10-01 07:57:07 -0400

    [Important] Removed 'React.StrictMode' for demo

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
     <Router history={history}>
       <App />
-    </Router>
-  </React.StrictMode>,
+    </Router>,
   document.getElementById('root')
 );
 
