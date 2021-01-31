353a836f6cf4b70fc969028ff36d8fcd511feeb1
kylejb <47502769+kylejb@users.noreply.github.com>
2021-01-31 00:48:42 -0500

    Removed comment.

--- src/Components/HeaderSearch.jsx
+++ src/Components/HeaderSearch.jsx
@@ -2,13 +2,6 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Dropdown } from 'semantic-ui-react';
 
-//* add country, state to db
-// const beachOptions = [
-//   // { key: 'globe', value: 'globe', flag: 'world', text: 'Earth' },
-//   { key: 'rbny', value: 'rockaway beach', flag: 'us', text: 'Rockaway Beach, NY' },
-//   { key: 'lbny', value: 'long beach', flag: 'us', text: 'Long Beach, NY' },
-//   { key: 'tobc', value: 'tofino', flag: 'ca', text: 'Tofino, BC' },
-// ]
 
 const renderBeachOption = (beachObj) => {
   return { key: beachObj.id, value: beachObj.name, flag: 'us', text: beachObj.name }
